883886760722c7ae150c2869ed5e6e9f5beb8ff2
Flavio Martins <flaviomartins@acm.org>
2020-03-23 13:37:06 +0000

    Task now does a Save Page Now to IA.

--- tasks.py
+++ tasks.py
@@ -1,3 +1,6 @@
+import logging
+
+import savepagenow
 from redis import Redis
 from rq import Queue
 from surt import surt
@@ -9,8 +12,26 @@ SURT_SORTED_SET_KEY = f"{PREFIX}:surt"
 
 redis_conn = Redis()
 
+log = logging.getLogger(__name__)
+
 
 def surt_task(url):
     surted = surt(url)
     response = redis_conn.zincrby(SURT_SORTED_SET_KEY, 1, surted)
+
+    """
+        Saves a URL with the Wayback Machine.
+        """
+
+    # Sent URL to Internet Archive
+    # This is required so we throw an error if it fails
+    log.debug(f"Archiving {url} to IA")
+    try:
+        ia_url, ia_captured = savepagenow.capture_or_cache(
+            url, user_agent="revisionista (https://revisionista.pt)"
+        )
+        log.info(f"Saving {url} memento URL {ia_url}")
+    except Exception as e:
+        log.error(e)
+
     return response
